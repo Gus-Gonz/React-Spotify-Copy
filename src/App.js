b5af2f712e51fe900912e8a3e22cfb00ae21e9b6
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-02-16 13:23:41 -0300

    integramos el Login en nuestra app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import React, { useEffect } from "react";
+import Login from "./pages/Login/index";
+import { getTokenFromUrl } from "./pages/Login/utills";
+
 function App() {
-  return <div></div>;
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    const token = hash.access_token;
+    window.location.hash = "";
+    console.log("I have the token !!", hash);
+  }, []);
+
+  return <Login />;
 }
 
 export default App;
