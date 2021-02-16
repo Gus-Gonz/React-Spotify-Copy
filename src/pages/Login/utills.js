87d2d15202b8a360af5b19925dae21dfb4e539d6
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-02-16 12:36:45 -0300

    Se construyo el Url utilizado para el Login con Spotify

---
+++ src/pages/Login/utills.js
@@ -0,0 +1,20 @@
+// https://developer.spotify.com/documentation/web-api/quick-start/
+//This might help you to understand what I have done here
+
+export const authEndPoint = "https://accounts.spotify.com/authorize";
+
+const redirectUrl = "http://localhost:3000";
+
+const clientId = "cbb9d3995fd44366ba7801a16db5f36c";
+
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+
+export const spotifyLoginUrl = `${authEndPoint}?client_id=${clientId}&redirect_uri=${redirectUrl}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
