bdccd8801853d2692f2675e8040cfe036fe12054
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-02-16 14:04:43 -0300

    Se implemento redux en la pagina Login

--- src/App.js
+++ src/App.js
@@ -3,13 +3,6 @@ import Login from "./pages/Login/index";
 import { getTokenFromUrl } from "./pages/Login/utills";
 
 function App() {
-  useEffect(() => {
-    const hash = getTokenFromUrl();
-    const token = hash.access_token;
-    window.location.hash = "";
-    console.log("I have the token !!", hash);
-  }, []);
-
   return <Login />;
 }
 
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import logo from "../../assets/Logo/spotify-logo.svg";
 
 import { StyledDiv, StyledImg, StyledA } from "./styles";
 import { spotifyLoginUrl } from "./utills";
 
-const Login = () => {
+import { connect } from "react-redux";
+import * as loginActions from "../../store/actions/index";
+
+import { getTokenFromUrl } from "./utills";
+
+const Login = (props) => {
+  useEffect(() => {
+    if (window.location.hash) {
+      const hash = getTokenFromUrl();
+      const token = hash.access_token;
+      // window.location.hash = "";
+      props.onInitToken(token);
+    } else {
+      return;
+    }
+  }, [window.location.hash]);
+
   return (
     <StyledDiv>
       <StyledImg alt="Spotify-Logo" src={logo}></StyledImg>
@@ -13,4 +29,10 @@ const Login = () => {
   );
 };
 
-export default Login;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onInitToken: (token) => dispatch(loginActions.setSpotifyToken(token)),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(Login);
