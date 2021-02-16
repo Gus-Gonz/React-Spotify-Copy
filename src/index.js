f519c7a95062edb0bb77838528af3938ac212d8d
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-02-16 13:46:41 -0300

    Se agregaron las importaciones pertinenetes y el set up de Redux

--- src/index.js
+++ src/index.js
@@ -2,11 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import loginReducer from "./store/reducer/login";
+
 import "./index.css";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  loginReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById("root")
 );
