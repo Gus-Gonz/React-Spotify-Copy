894c45b2dc62ea209ebef1862d57af16bf6e73f5
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-02-16 12:37:05 -0300

    Se agrego el link a la pagina

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import logo from "../../assets/Logo/spotify-logo.svg";
 
 import { StyledDiv, StyledImg, StyledA } from "./styles";
+import { spotifyLoginUrl } from "./utills";
 
 const Login = () => {
   return (
     <StyledDiv>
       <StyledImg alt="Spotify-Logo" src={logo}></StyledImg>
-      <StyledA>LOGIN WITH SPOTIFY</StyledA>
+      <StyledA href={spotifyLoginUrl}>LOGIN WITH SPOTIFY</StyledA>
     </StyledDiv>
   );
 };
