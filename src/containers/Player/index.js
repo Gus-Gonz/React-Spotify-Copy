1ba640036dc6dd43d3171c4d546e09e35f488991
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-02-17 13:14:00 -0300

    Estructura y estilos basicos para el player

---
+++ src/containers/Player/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyledDiv } from "./styles";
+
+const body = () => {
+  return (
+    <StyledDiv>
+      <h1>WE ARE THE BODY</h1>
+    </StyledDiv>
+  );
+};
+
+export default body;
---
+++ src/containers/Player/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledDiv = styled.div`
+  border: solid 1px red;
+  flex: 0.8;
+  height: 100vh;
+  background: linear-gradient(rgb(91, 87, 115), rgba(0, 0, 0, 1));
+  color: white;
+  padding: 30px;
+`;
