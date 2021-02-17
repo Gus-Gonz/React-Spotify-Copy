99e19b93dc265a03dd80da73ca2dbbc57c82f52a
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-02-17 13:13:48 -0300

    Estructura y estilos basicos para el sidebar

---
+++ src/components/SideBar/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { StyledDiv } from "./styles";
+
+const sideBar = () => {
+  return (
+    <StyledDiv>
+      <li>THIS IS THE SIDE BAR</li>
+    </StyledDiv>
+  );
+};
+
+export default sideBar;
---
+++ src/components/SideBar/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledDiv = styled.div`
+  border: 1px solid black;
+  height: 100vh;
+  flex: 0.2;
+  background: #040404;
+  color: white;
+  min-width: 220px;
+  padding: 20px 20px;
+`;
