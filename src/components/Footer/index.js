2e47649027e04f39cde662ef0c83ee199e872210
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-02-17 13:13:14 -0300

    Estructura y estilos basicos para Footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { StyledDiv } from "./styles";
+
+const footer = () => {
+  return (
+    <StyledDiv>
+      <h1> we are the footer</h1>
+    </StyledDiv>
+  );
+};
+
+export default footer;
---
+++ src/components/Footer/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledDiv = styled.div`
+  position: fixed;
+  bottom: 0;
+  height: 65px;
+  width: 100%;
+  background-color: #282828;
+  padding: 20px 15 px;
+`;
