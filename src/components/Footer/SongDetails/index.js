1ff265c3d3ac8473059cbadf6005d8cd0f1152b8
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-02-18 11:53:41 -0300

    Estructura basica del componente SongDetails

---
+++ src/components/Footer/SongDetails/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { StyledDiv } from "./styles";
+
+const songDetails = () => {
+  return (
+    <StyledDiv>
+      <img />
+      <div>
+        <h4>Yeah!!</h4>
+        <p>...</p>
+      </div>
+    </StyledDiv>
+  );
+};
+
+export default songDetails;
---
+++ src/components/Footer/SongDetails/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const StyledDiv = styled.div`
+  display: flex;
+  align-items: center;
+  color: white;
+  width: 300px;
+
+  &img {
+    height: 60px;
+    width: 60px;
+    margin-right: 20px;
+    object-fit: contain;
+  }
+`;
